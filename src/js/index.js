eb90fa83f0540848ea45e414ca43733badd51b83
Jin Chen <jinchen93@gmail.com>
2017-06-18 00:58:13 -0700

    Add responsive nav tabs

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,25 @@
+import Vue from './vue.min.js';
 require("../scss/index.scss");
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('loaded');
+  const app = new Vue({
+    el: ".page-wrapper",
+    data: {
+      page: 'ABOUT'
+    },
+    methods: {
+      about() {
+        return this.page === 'ABOUT';
+      },
+      work() {
+        return this.page === 'WORK';
+      },
+      clickAbout() {
+        this.page = 'ABOUT';
+      },
+      clickWork() {
+        this.page = 'WORK';
+      }
+    }
+  });
 });
