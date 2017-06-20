0f81da3510a8c2b9b093f169bca0ac068f85a0a9
Jin Chen <jinchen93@gmail.com>
2017-06-19 17:48:57 -0700

    Update test again using anonymous functions

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const Vue = require("Vue");
 const App = require("../src/js/app");
 const assert = require("assert");
 
-describe("App", () => {
+describe("App", function() {
   it("is a Vue instance", function() {
     const app = App();
     assert(app instanceof Vue);
